dbe1b1b49927e41897bbb448aaf029eba48b075f
JoeLorenzo <Joselmedrano@gmail.com>
2019-09-04 17:40:58 -0500

    -five components rendered.
    -added basic styled-componets.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 import People from './components/people'
 
 
@@ -10,11 +11,17 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [apiUrl, setApiUrl] = useState("https://swapi.co/api/people/")
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <People />
+      <People url={apiUrl + "1"} />
+      <People url={apiUrl + "2"} />
+      <People url={apiUrl + "1"} />
+      <People url={apiUrl + "3"} />
+      <People url={apiUrl + "4"} />
+      <People url={apiUrl + "5"} />
     </div>
   );
 }
--- starwars/src/components/people.js
+++ starwars/src/components/people.js
@@ -1,18 +1,35 @@
 import React, { useState, useEffect } from 'react';
+import App from "../App"
 import axios from 'axios';
+import { Jumbotron } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styled from "styled-components"
 
+const NameTag = styled.h2`
+font-size: 1rem;
+color:blue;
+`
 
+function People(props){
+  const [charName, setCharName] = useState("")
+  const [charGender, setCharGender] = useState("")
+  const [birthYear, setBirthYear] = useState("")
 
-function People(){
+  useEffect( () => {
+    axios.get(props.url)
+    .then(res => {
+      setCharName(res.data.name)
+      setCharGender(res.data.gender)
+      setBirthYear(res.data.birth_year)
+    })
+  }, [])
+
   return (
     <div>
-      <h2>Hello World</h2>
-      useEffect(() => {
-        axios.get("")
-        .then(res => {
-        console.log(res.data)
-  })
-}, [])
+      <Jumbotron>
+      <NameTag> {charName} </NameTag>
+       <p> Gender: {charGender}, born in the year {birthYear}  </p>
+      </Jumbotron>
     </div>
   )
 }
